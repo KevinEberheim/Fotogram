7b7792ef7c1937239482ef51731599f0c7821c88
KevinEberheim <kevin.eberheim@gmail.com>
2025-12-09 16:16:23 +0100

    add dialog in index.html add dialog functions in script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-let images = ["eintracht.jpg", "eintracht.jpg", "eintracht.jpg", "eintracht.jpg", "eintracht.jpg", "eintracht.jpg", "eintracht.jpg"];
+let images = ["eintracht.jpg", "favicon.svg", "eintracht.jpg", "eintracht.jpg", "eintracht.jpg", "eintracht.jpg", "eintracht.jpg"];
 
 function init() {    
-let imageRef = document.getElementById('image_load')
+let imageRef = document.getElementById('image_load');
 for (let index = 0; index < images.length; index++) {
     imageRef.innerHTML += load_images(index);
     
@@ -9,5 +9,54 @@ for (let index = 0; index < images.length; index++) {
 }
 
 function load_images(index) {
+    return `<img onclick="openDialog(${index})" src=./img/${images[index]}>`;
+}
+
+const dialogRef= document.getElementById("pictureDialog");
+const dialogFooterRef = document.getElementById("dialogFooter")
+function openDialog(index){
+    dialogRef.showModal();
+    showImageDialog(index);
+    dialogFooterRef.innerHTML = footerdialog(index);
+}
+
+function closeDialog(){
+    dialogRef.close();
+}
+
+function showImageDialog(index){
+    let showImageRef = document.getElementById("dialogMain");
+    showImageRef.innerHTML = showImage(index);
+}
+
+function showImage(index){
     return `<img src=./img/${images[index]}>`;
+}
+
+function buttonLeft(index){
+    index--;
+    if (index>=0) {
+        openDialog(index);
+    }
+    else{
+        index=images.length-1
+        openDialog(index);
+    }
+}
+
+function buttonRight(index){
+    index++;
+    if (index<images.length) {
+        openDialog(index);
+    }
+    else{
+        index=0;
+        openDialog(index);
+    }
+}
+
+function footerdialog(index){
+    return `<button onclick="buttonLeft(${index})">links</button>
+                <span>${index+1}/${images.length}</span>
+                <button onclick="buttonRight(${index})">rechts</button>`
 }
