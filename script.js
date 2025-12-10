0aa5db338503ad15baea0f20cf7790648dd2764c
KevinEberheim <kevin.eberheim@gmail.com>
2025-12-10 02:03:30 +0100

    add images for album

--- script.js
+++ script.js
@@ -1,72 +1,83 @@
-let images = ["eintracht.jpg", "favicon.svg", "eintracht.jpg", "eintracht.jpg", "eintracht.jpg", "eintracht.jpg", "eintracht.jpg"];
+let images = ["christmas.jpg",
+    "christmas_cookies.jpg",
+    "christmas_gifts.jpg",
+    "christmas_tree.jpg",
+    "christmas_village.jpg",
+    "merry_christmas.jpg",
+    "moon.png",
+    "santa_claus.jpg",
+    "snowflake.jpg",
+    "star.jpg",
+    "winter.jpg",
+    "winter_landscape.jpg"];
 
-function init() {    
-let imageRef = document.getElementById('image_load');
-for (let index = 0; index < images.length; index++) {
-    imageRef.innerHTML += load_images(index);
-    
-}
+function init() {
+    let imageRef = document.getElementById('image_load');
+    for (let index = 0; index < images.length; index++) {
+        imageRef.innerHTML += load_images(index);
+
+    }
 }
 
 function load_images(index) {
     return `<img onclick="openDialog(${index}), eventBubbling(event)" tabindex="0" src=./img/${images[index]}>`;
 }
 
-const dialogRef= document.getElementById("pictureDialog");
+const dialogRef = document.getElementById("pictureDialog");
 const dialogFooterRef = document.getElementById("dialogFooter")
-function openDialog(index){
+function openDialog(index) {
     dialogRef.showModal();
     showImageDialog(index);
     dialogFooterRef.innerHTML = footerdialog(index);
     name_for_img(index);
 }
 
-function closeDialog(){
+function closeDialog() {
     dialogRef.close();
 }
 
-function showImageDialog(index){
+function showImageDialog(index) {
     let showImageRef = document.getElementById("dialogMain");
     showImageRef.innerHTML = showImage(index);
 }
 
-function showImage(index){
+function showImage(index) {
     return `<img src=./img/${images[index]}>`;
 }
 
-function buttonLeft(index){
+function buttonLeft(index) {
     index--;
-    if (index>=0) {
+    if (index >= 0) {
         openDialog(index);
     }
-    else{
-        index=images.length-1
+    else {
+        index = images.length - 1
         openDialog(index);
     }
 }
 
-function buttonRight(index){
+function buttonRight(index) {
     index++;
-    if (index<images.length) {
+    if (index < images.length) {
         openDialog(index);
     }
-    else{
-        index=0;
+    else {
+        index = 0;
         openDialog(index);
     }
 }
 
-function footerdialog(index){
+function footerdialog(index) {
     return `<button onclick="buttonLeft(${index})">&blacktriangleleft;</button>
-                <span>${index+1}/${images.length}</span>
+                <span>${index + 1}/${images.length}</span>
                 <button onclick="buttonRight(${index})">&blacktriangleright;</button>`
 }
 
-function eventBubbling(event){
+function eventBubbling(event) {
     event.stopPropagation();
 }
 
 const image_name = document.getElementById('name_img')
-function name_for_img(index){
+function name_for_img(index) {
     image_name.innerHTML = images[index];
 }
